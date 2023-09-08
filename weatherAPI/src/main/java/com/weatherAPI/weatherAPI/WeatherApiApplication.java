ea1a14739c4ae27018eef753f879c0194b260db5
Blake Joye <tbjoye@uncg.edu>
2023-09-08 00:20:29 -0400

    commit https://github.com/blakejoye/prototypeAPI.git

--- weatherAPI/src/main/java/com/weatherAPI/weatherAPI/WeatherApiApplication.java
+++ weatherAPI/src/main/java/com/weatherAPI/weatherAPI/WeatherApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+//
 @SpringBootApplication
 public class WeatherApiApplication {
 
